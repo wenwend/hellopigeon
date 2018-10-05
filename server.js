77654b0b56a49e97b89e1a77100a5ac86fcfe2cb
Rachel Dong <racheldong@MacBook0309.local>
2018-10-05 16:29:28 -0700

    add clearDB from heroku

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const app = express();
+var mysql=require('mysql');
 var http = require('http');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const cors = require('cors')
+
 
 var corsOptions = {
   origin: 'http://localhost:8080',
   optionsSuccessStatus: 200 
 }
 app.use(cors(corsOptions))
+
+
+//get ClearDB with server
+var con=mysql.createConnection({
+	//CLEARDB_DATABASE_URL = mysql://bfbb416be2add3:333fa482@us-cdbr-iron-east-01.cleardb.net/heroku_a68062f9c8baf13?reconnect=true;
+	host:"us-cdbr-iron-east-01.cleardb.net",
+	user:"bfbb416be2add3",
+	password:"333fa482",
+	database:"heroku_a68062f9c8baf13"
+})
+
+con.connect(function(err){
+	if (err) throw err;
+	con.query("SELECT * FROM pigeons", function (err, result, fields) {
+	if (err) throw err;
+	console.log(result);
+  });
+	//console.log("You are connected with DB!");
+})
 
 
 //get all 
@@ -42,5 +63,5 @@ app.route('/api/pigeons/:name').delete((req, res) => {
 
 //start listening
 app.listen(8080, function() {
-    console.log("Server running");
+	console.log("Server running");
 });
